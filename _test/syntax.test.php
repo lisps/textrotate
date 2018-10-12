55a988d244a025cae91475e6d7478ceb796a4bb1
peterfromearth <coder@peterfromearth.de>
2018-10-12 21:42:55 +0200

    fix test

--- _test/syntax.test.php
+++ _test/syntax.test.php
@@ -17,10 +17,8 @@ class plugin_textrotate_syntax_test extends DokuWikiTest {
     public function test_basic_syntax() {
         $xhtml = p_wiki_xhtml('test:plugin_textrotate');
         $doc = phpQuery::newDocument($xhtml);
-
-        $img = pq('img',$doc);
         
-        echo $doc;
+        $this->assertContains('<img class="textrotate" src="lib/images/tmp/', $doc);
 
     }
     
